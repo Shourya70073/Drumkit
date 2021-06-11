9079e08af7aba10efcad16eebb64f7cc5f5c56f1
Shourya70073 <71991617+Shourya70073@users.noreply.github.com>
2021-06-11 13:43:48 +0530

    Update java.js

--- java.js
+++ java.js
@@ -54,45 +54,45 @@ function handleClick(){
 }
 
 document.addEventListener("keydown",function(event){
-  if(event.key=="w"){
+  if(event.key=="w" || event.key=="W"){
     var audio= new Audio("tom-1.mp3");
     audio.play();
       animate(event.key);
   }
-  else if(event.key=="a"){
+  else if(event.key=="a" || event.key=="A"){
     var audio= new Audio("tom-2.mp3");
     audio.play();
       animate(event.key);
   }
-  else if(event.key=="s"){
+  else if(event.key=="s" || event.key=="S"){
     var audio= new Audio("tom-3.mp3");
     audio.play();
       animate(event.key);
   }
-  else if(event.key=="d"){
+  else if(event.key=="d" || event.key=="D"){
     var audio= new Audio("tom-4.mp3");
     audio.play();
       animate(event.key);
   }
-  else if(event.key=="j"){
+  else if(event.key=="j" || event.key=="J"){
     var audio= new Audio("kick-bass.mp3");
     audio.play();
       animate(event.key);
   }
-  else if(event.key=="k"){
+  else if(event.key=="k" || event.key=="K"){
     var audio= new Audio("snare.mp3");
     audio.play();
       animate(event.key);
   }
-  else if(event.key=="l"){
+  else if(event.key=="l" || event.key=="L") {
     var audio= new Audio("crash.mp3");
     audio.play();
     animate(event.key);
   }
 
+}
 
-
-});
+);
 function animate(ckey){
   var q=document.querySelector("."+ckey);
   q.classList.add("pressed");
